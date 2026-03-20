ef356d0168a6c40a970acc6b521c5ee55caef3f5
princess-chaea <dltjdrms320@gmail.com>
2026-03-20 14:41:59 +0900

    Add files via upload

--- src/types/math.d.ts
+++ src/types/math.d.ts
@@ -3,7 +3,15 @@ import * as React from 'react';
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "math-field": any;
+      'math-field': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        ref?: React.RefObject<any>;
+        style?: React.CSSProperties;
+        multiline?: string;
+        'smart-mode'?: string;
+        'smart-fence'?: string;
+        'math-virtual-keyboard-policy'?: string;
+        placeholder?: string;
+      };
     }
   }
 }
