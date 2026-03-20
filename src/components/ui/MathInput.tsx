7c44bf733cf57a5147c76699329f2ebec7a2d2fb
princess-chaea <dltjdrms320@gmail.com>
2026-03-20 14:37:39 +0900

    Add files via upload

--- src/components/ui/MathInput.tsx
+++ src/components/ui/MathInput.tsx
@@ -3,6 +3,22 @@
 import React, { useEffect, useRef } from "react";
 import { useMathKeypad } from "./MathKeypadContext";
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'math-field': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        ref?: React.RefObject<any>;
+        style?: React.CSSProperties;
+        multiline?: string;
+        'smart-mode'?: string;
+        'smart-fence'?: string;
+        'math-virtual-keyboard-policy'?: string;
+        placeholder?: string;
+      };
+    }
+  }
+}
+
 interface MathInputProps {
   value: string;
   onChange: (value: string) => void;
