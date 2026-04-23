c78c9053a7ed8ff9f1dbae32f1de85d351d6fb96
princess-chaea <dltjdrms320@gmail.com>
2026-04-23 22:31:48 +0900

    Add files via upload

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useRef } from 'react';
 import { supabase } from '@/lib/supabase';
 
 export interface Game {
